364465f40d58f5a2c944378395d4f16b1486d054
Chris Eddy <ChrisEddy@users.noreply.github.com>
2018-06-20 21:42:10 -0700

    buttuns are linked, removed logo, aligned better
    change gradeint too

--- src/components/titlePage.js
+++ src/components/titlePage.js
@@ -7,40 +7,55 @@ class TitlePage extends Component {
     render() {
         return (
             <div className="titlePage">
-                <img className='exokitLogo' src={exokitLogo}/>
+
                 <div className='titleText'>
+
                     <div className='row'>
                         <div className='col-12'>
-                            <h1 className='display-4'>Exokit is a new, fast post-screen era browser.</h1>
+                            <h1 className='display-4'>Exokit is a new, fast, post-screen era browser.</h1>
                         </div>
                     </div>
+
                     <br/>
-                    <div className='row'>
-                        <div className='col-3'>
 
+                    <div className='row buttonRow'>
+                        <div className='col-lg-6 col-md-6 col-sm-12 col-xs-12'>
+                            <a href='https://threejs.org/'>
+                                <button type="button" className="btn btn-outline-primary btn-lg">Demos</button>
+                            </a>
                         </div>
-                        <div className='col-3'>
-                            <button type="button" className="btn btn-outline-primary btn-lg">Demos</button>
-                        </div>
-
-                        <div className='col-3'>
-                            <button type="button" className="btn btn-outline-dark btn-lg">Github</button>
-                        </div>
-                        <div className='col-3'>
-
+                        <div className='col-lg-6 col-md-6 col-sm-12 col-xs-12'>
+                            <a href='https://github.com/webmixedreality/exokit'>
+                                <button type="button" className="btn btn-outline-dark btn-lg">Github</button>
+                            </a>
                         </div>
                     </div>
+
                     <br/>
+
                     <div className='row'>
                         <div className='col-12'>
-                            <i className="fab fa-github fa-3x"/>
-                            <i className="fab fa-twitter fa-3x"/>
-                            <i className="fab fa-facebook fa-3x"/>
-                            <i className="fab fa-instagram fa-3x"/>
-                            <i className="fab fa-linkedin fa-3x"/>
-                            <i className="fab fa-youtube fa-3x"/>
+                            <a href='https://github.com/webmixedreality/exokit'>
+                                <i className="fab fa-github fa-3x"/>
+                            </a>
+                            <a href='https://twitter.com/webmixedreality'>
+                                <i className="fab fa-twitter fa-3x"/>
+                            </a>
+                            <a href=''>
+                                <i className="fab fa-facebook fa-3x"/>
+                            </a>
+                            <a href='https://www.instagram.com/exokitbrowser/?hl=en'>
+                                <i className="fab fa-instagram fa-3x"/>
+                            </a>
+                            <a href=''>
+                                <i className="fab fa-linkedin fa-3x"/>
+                            </a>
+                            <a href=''>
+                                <i className="fab fa-youtube fa-3x"/>
+                            </a>
                         </div>
                     </div>
+
                 </div>
             </div>
         );
