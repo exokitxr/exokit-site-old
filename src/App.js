aaf32bf36998dac237944a2aa20f77df3392a1f2
Chris Eddy <ChrisEddy@users.noreply.github.com>
2018-06-21 14:56:37 -0700

    blank section example

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import TopNavbar from './components/navbar';
 import AppGrid from './components/appGrid';
 import TitlePage from "./components/titlePage";
 import DemoPage from "./components/demoPage";
+import NewPage from "./components/newPage";
 
 class App extends Component {
     render() {
@@ -13,6 +14,7 @@ class App extends Component {
                 <TopNavbar/>
                 <TitlePage/>
                 <DemoPage/>
+                <NewPage/>
                 {/*<Jumbotron>*/}
                     {/*<h1 className="display-4">Exokit</h1>*/}
                     {/*<p className='lead'>The fastest and most portable VR/AR toolbox for developers.</p>*/}
---
+++ src/components/newPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../css/newPage.css';
+
+class NewPage extends Component {
+    render() {
+        return (
+            <div className='newPage'>
+                <p>hello</p>
+            </div>
+        );
+    }
+}
+export default NewPage;
