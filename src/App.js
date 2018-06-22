7ea1d87b361d3416c1b2a505489d924543fe143e
Avaer Kazmer <a@modules.io>
2018-06-21 22:38:43 -0400

    Add multiplayer section

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import TitlePage from "./components/titlePage";
 import DemoPage from "./components/demoPage";
 import FeaturesPage from "./components/featuresPage";
 import FeaturesPageNav from "./components/featuresPageNav";
+import MultiplayerPage from "./components/multiplayerPage";
 import NewsPageNav from "./components/newsPage";
 
 class App extends Component {
@@ -20,12 +21,13 @@ class App extends Component {
         <FeaturesPageNav/>
         <FeaturesPageNav/>
         <FeaturesPageNav/>
+        <MultiplayerPage/>
         <NewsPageNav/>
         <Jumbotron>
           <h1 className="display-4">Exokit</h1>
           <p className='lead'>The fastest and most portable VR/AR toolbox for developers.</p>
         </Jumbotron>
-        <AppGrid/>
+        {/* <AppGrid/> */}
       </div>
     );
   }
---
+++ src/components/multiplayerPage.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import '../css/multiplayerPage.css';
+
+import portalImage from '../images/portals.png'
+import mushroomImage from '../images/mushrooms.png'
+import minecraftAvatarImage from '../images/minecraftAvatar.png'
+import girlAvatarImage from '../images/girlAvatar.png'
+import shapeBuilderImage from '../images/shapeBuilder.png'
+import zeoForestImage from '../images/zeoForest.png'
+
+class MultiplayerPage extends Component {
+  render() {
+    return (
+      <div className='multiplayerPage'>
+        <h1 className='heading display-4'>Multiplayer servers</h1>
+        <p>Click one to enter</p>
+        <div className='container'>
+          <ul className="list-group">
+            <li className="list-group-item d-flex">
+              <img className="server-img" src={portalImage} alt="Card image cap"/>
+              <div className='server-name'>
+                <div className='server-name-title'>Magic server one</div>
+                <p>A place to serve magic</p>
+              </div>
+              <div className='server-stat'><i class="fas fa-user"></i>2/8</div>
+              <div className='server-stat'><i class="fas fa-toggle-on"></i>3</div>
+            </li>
+            <li className="list-group-item d-flex">
+              <img className="server-img" src={mushroomImage} alt="Card image cap"/>
+              <div className='server-name'>
+                <div className='server-name-title'>Magic server two</div>
+                <p>A place to serve magic</p>
+              </div>
+              <div className='server-stat'><i class="fas fa-user"></i>2/8</div>
+              <div className='server-stat'><i class="fas fa-toggle-on"></i>3</div>
+            </li>
+            <li className="list-group-item d-flex">
+              <img className="server-img" src={minecraftAvatarImage} alt="Card image cap"/>
+              <div className='server-name'>
+                <div className='server-name-title'>Fail whale zone</div>
+                <p>A place to serve magic</p>
+              </div>
+              <div className='server-stat'><i class="fas fa-user"></i>2/8</div>
+              <div className='server-stat'><i class="fas fa-toggle-on"></i>3</div>
+            </li>
+            <li className="list-group-item d-flex">
+              <img className="server-img" src={shapeBuilderImage} alt="Card image cap"/>
+              <div className='server-name'>
+                <div className='server-name-title'>Ice cream cone</div>
+                <p>A place to serve magic</p>
+              </div>
+              <div className='server-stat'><i class="fas fa-user"></i>2/8</div>
+              <div className='server-stat'><i class="fas fa-toggle-on"></i>3</div>
+            </li>
+            <li className="list-group-item d-flex">
+              <img className="server-img" src={zeoForestImage} alt="Card image cap"/>
+              <div className='server-name'>
+                <div className='server-name-title'>Why my alone</div>
+                <p>A place to serve magic</p>
+              </div>
+              <div className='server-stat'><i class="fas fa-user"></i>2/8</div>
+              <div className='server-stat'><i class="fas fa-toggle-on"></i>3</div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+export default MultiplayerPage;
