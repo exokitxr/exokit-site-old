db03f6306d0d901a986f9f112ceb1740634d26f7
Chris Eddy <ChrisEddy@users.noreply.github.com>
2018-06-21 23:16:44 -0700

    half-done nav script, grid work

--- src/components/featuresPage.js
+++ src/components/featuresPage.js
@@ -15,83 +15,67 @@ class FeaturesPage extends Component {
           <div className='col-sm-8'>
             <div className='appGrid row d-flex'>
               <div className='appCards row col-12 flex-grow-1'>
-                <div className='col-2'>
-                  <div className='appCard'>
-                    <img src={portalImage} alt="Portal Demo" className="img-fluid"/>
-                    <div className='title'>Title</div>
+                <div className='row'>
+                  <div className='col-lg-3 col-md-3 col-sm-6 col-xs-6'>
+                    <div className='appCard'>
+                      <img src={portalImage} alt="Portal Demo" className="img-fluid"/>
+                      <div className='title'>Title</div>
+                    </div>
+                  </div>
+                  <div className='col-lg-3 col-md-3 col-sm-6 col-xs-6'>
+                    <div className='appCard'>
+                      <img src={mushroomImage} alt="VR Guy" className="img-fluid"/>
+                      <div className='title'>Title</div>
+                    </div>
+                  </div>
+                  <div className='col-lg-3 col-md-3 col-sm-6 col-xs-6'>
+                    <div className='appCard'>
+                      <img src={minecraftAvatarImage} alt="VR Guy" className="img-fluid"/>
+                      <div className='title'>Title</div>
+                    </div>
+                  </div>
+                  <div className='col-lg-3 col-md-3 col-sm-6 col-xs-6'>
+                    <div className='appCard'>
+                      <img src={portalImage} alt="Portal Demo" className="img-fluid"/>
+                      <div className='title'>Title</div>
+                    </div>
                   </div>
                 </div>
-                <div className='col-2'>
-                  <div className='appCard'>
-                    <img src={mushroomImage} alt="VR Guy" className="img-fluid"/>
-                    <div className='title'>Title</div>
+
+                <div className='row'>
+                  <div className='ccol-lg-3 col-md-3 col-sm-6 col-xs-6'>
+                    <div className='appCard'>
+                      <img src={portalImage} alt="Portal Demo" className="img-fluid"/>
+                      <div className='title'>Title</div>
+                    </div>
+                  </div>
+                  <div className='col-lg-3 col-md-3 col-sm-6 col-xs-6'>
+                    <div className='appCard'>
+                      <img src={mushroomImage} alt="VR Guy" className="img-fluid"/>
+                      <div className='title'>Title</div>
+                    </div>
+                  </div>
+                  <div className='col-lg-3 col-md-3 col-sm-6 col-xs-6'>
+                    <div className='appCard'>
+                      <img src={minecraftAvatarImage} alt="VR Guy" className="img-fluid"/>
+                      <div className='title'>Title</div>
+                    </div>
+                  </div>
+                  <div className='col-lg-3 col-md-3 col-sm-6 col-xs-6'>
+                    <div className='appCard'>
+                      <img src={portalImage} alt="Portal Demo" className="img-fluid"/>
+                      <div className='title'>Title</div>
+                    </div>
                   </div>
                 </div>
-                <div className='col-2'>
-                  <div className='appCard'>
-                    <img src={minecraftAvatarImage} alt="VR Guy" className="img-fluid"/>
-                    <div className='title'>Title</div>
-                  </div>
-                </div>
-                <div className='col-2'>
-                  <div className='appCard'>
-                    <img src={portalImage} alt="Portal Demo" className="img-fluid"/>
-                    <div className='title'>Title</div>
-                  </div>
-                </div>
-                <div className='col-2'>
-                  <div className='appCard'>
-                    <img src={mushroomImage} alt="VR Guy" className="img-fluid"/>
-                    <div className='title'>Title</div>
-                  </div>
-                </div>
-                <div className='col-2'>
-                  <div className='appCard'>
-                    <img src={minecraftAvatarImage} alt="VR Guy" className="img-fluid"/>
-                    <div className='title'>Title</div>
-                  </div>
-                </div>
-                <div className='col-2'>
-                  <div className='appCard'>
-                    <img src={portalImage} alt="Portal Demo" className="img-fluid"/>
-                    <div className='title'>Title</div>
-                  </div>
-                </div>
-                <div className='col-2'>
-                  <div className='appCard'>
-                    <img src={mushroomImage} alt="VR Guy" className="img-fluid"/>
-                    <div className='title'>Title</div>
-                  </div>
-                </div>
-                <div className='col-2'>
-                  <div className='appCard'>
-                    <img src={minecraftAvatarImage} alt="VR Guy" className="img-fluid"/>
-                    <div className='title'>Title</div>
-                  </div>
-                </div>
-                <div className='col-2'>
-                  <div className='appCard'>
-                    <img src={portalImage} alt="Portal Demo" className="img-fluid"/>
-                    <div className='title'>Title</div>
-                  </div>
-                </div>
-                <div className='col-2'>
-                  <div className='appCard'>
-                    <img src={mushroomImage} alt="VR Guy" className="img-fluid"/>
-                    <div className='title'>Title</div>
-                  </div>
-                </div>
-                <div className='col-2'>
-                  <div className='appCard'>
-                    <img src={minecraftAvatarImage} alt="VR Guy" className="img-fluid"/>
-                    <div className='title'>Title</div>
-                  </div>
-                </div>
+
               </div>
+
               <div className="alert alert-success">
                 <h4 className="alert-heading">Cards footer</h4>
                 <p className="mb-0">Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>
               </div>
+
             </div>
           </div>
         </div>
