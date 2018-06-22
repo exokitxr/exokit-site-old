26f03706ad4a0356d44e62d8acaf191ce38cded1
Chris Eddy <ChrisEddy@users.noreply.github.com>
2018-06-22 01:14:04 -0700

    navbar right justified + added JQ and popper

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -18,12 +18,12 @@ class TopNavbar extends Component {
                   aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"/>
           </button>
-          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
             <div className="navbar-nav">
-              <a className="nav-item nav-link active" href="#">Home<span className="sr-only">(current)</span></a>
-              <a className="nav-item nav-link" href="https://discord.gg/aQhHskF">Discord</a>
-              <a className="nav-item nav-link" href="https://www.twitch.tv/avaer">Twitch</a>
-              <a className="nav-item nav-link" href="https://get.webmr.io">Download</a>
+              <a className="nav-item nav-link active justify-content-end" href="#">Home<span className="sr-only">(current)</span></a>
+              <a className="nav-item nav-link justify-content-end" href="https://discord.gg/aQhHskF">Discord</a>
+              <a className="nav-item nav-link justify-content-end" href="https://www.twitch.tv/avaer">Twitch</a>
+              <a className="nav-item nav-link justify-content-end" href="https://get.webmr.io">Download</a>
             </div>
           </div>
         </nav>
