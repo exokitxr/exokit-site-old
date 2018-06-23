dfe275a6ccb983f85be898f9128407f3b98f1d86
Avaer Kazmer <a@modules.io>
2018-06-23 08:15:26 -0400

    Add missing news item keys

--- src/components/newsPage.js
+++ src/components/newsPage.js
@@ -15,10 +15,10 @@ class NewsPage extends Component {
       <div className='newsPage'>
         <h1 className='heading display-4'>News</h1>
 
-        {rows.map(row =>
-          <div className='row'>
-            {row.map(newsItem =>
-              <a className='col-lg-3 col-md-6 col-sm-12 col-xs-12' href={newsItem.url}>
+        {rows.map((row, i) =>
+          <div className='row' key={i}>
+            {row.map((newsItem, i) =>
+              <a className='col-lg-3 col-md-6 col-sm-12 col-xs-12' href={newsItem.url} key={i}>
                 <div className="card">
                   <img className="card-img-top" src={newsItem.img} alt="Card"/>
                   <div className="card-body">{newsItem.title}</div>
