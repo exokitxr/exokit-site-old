80e63e039637eff7c12529a22b593f8c14871021
Chris Eddy <ChrisEddy@users.noreply.github.com>
2018-07-02 15:00:36 -0700

    did some content writing, removed features sections

--- src/components/featuresPage.js
+++ src/components/featuresPage.js
@@ -22,13 +22,13 @@ class FeaturesPage extends Component {
                       <div className='col-lg-6 col-md-12 col-sm-12 col-xs-12 p-0'>
                         <div className='appCard'>
                           <img src={portalImage} alt="Portal Demo" className="img-fluid"/>
-                          <div className='title'>Title</div>
+                          <div className='title'>Reality Tabs</div>
                         </div>
                       </div>
                       <div className='col-lg-6 col-md-12 col-sm-12 col-xs-12 p-0'>
                         <div className='appCard'>
                           <img src={mushroomImage} alt="VR Guy" className="img-fluid"/>
-                          <div className='title'>Title</div>
+                          <div className='title'>Cross-Platform</div>
                         </div>
                       </div>
                     </div>
@@ -36,13 +36,13 @@ class FeaturesPage extends Component {
                       <div className='col-lg-6 col-md-6 col-sm-6 col-xs-6 p-0'>
                         <div className='appCard'>
                           <img src={menuImage} alt="Portal Demo" className="img-fluid"/>
-                          <div className='title'>Title</div>
+                          <div className='title'>Speed</div>
                         </div>
                       </div>
                       <div className='col-lg-6 col-md-6 col-sm-6 col-xs-6 p-0'>
                         <div className='appCard'>
                           <img src={lightsaberImage} alt="VR Guy" className="img-fluid"/>
-                          <div className='title'>Title</div>
+                          <div className='title'>Built in Javascript</div>
                         </div>
                       </div>
                     </div>
@@ -52,13 +52,13 @@ class FeaturesPage extends Component {
                       <div className='col-lg-6 col-md-12 col-sm-12 col-xs-12 p-0'>
                         <div className='appCard'>
                           <img src={mountainsImage} alt="Portal Demo" className="img-fluid"/>
-                          <div className='title'>Title</div>
+                          <div className='title'>Payment APIs</div>
                         </div>
                       </div>
                       <div className='col-lg-6 col-md-12 col-sm-12 col-xs-12 p-0'>
                         <div className='appCard'>
                           <img src={lavaImage} alt="VR Guy" className="img-fluid"/>
-                          <div className='title'>Title</div>
+                          <div className='title'>Game Development </div>
                         </div>
                       </div>
                     </div>
@@ -66,13 +66,13 @@ class FeaturesPage extends Component {
                       <div className='col-lg-6 col-md-6 col-sm-6 col-xs-6 p-0'>
                         <div className='appCard'>
                           <img src={snakeImage} alt="Portal Demo" className="img-fluid"/>
-                          <div className='title'>Title</div>
+                          <div className='title'>App Development</div>
                         </div>
                       </div>
                       <div className='col-lg-6 col-md-6 col-sm-6 col-xs-6 p-0'>
                         <div className='appCard'>
                           <img src={startZoneImage} alt="VR Guy" className="img-fluid"/>
-                          <div className='title'>Title</div>
+                          <div className='title'>A Social Experience</div>
                         </div>
                       </div>
                     </div>
@@ -80,8 +80,8 @@ class FeaturesPage extends Component {
                 </div>
 
               <div className="alert alert-success">
-                <h4 className="alert-heading">Cards footer</h4>
-                <p className="mb-0">Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>
+                <h4 className="alert-heading">{this.props.footerHeading}</h4>
+                <p className="mb-0 text-left">{this.props.footerBody}</p>
               </div>
       </div>
     );
